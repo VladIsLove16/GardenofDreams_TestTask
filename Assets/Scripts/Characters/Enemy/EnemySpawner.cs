a4d75126f9b7c7d34c41e17e6dafbabe0e9401b7
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2025-01-23 20:10:34 +0300

    refactoring

---
+++ Assets/Scripts/Characters/Enemy/EnemySpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<Transform> SpawnPositions;
+    [SerializeField] Enemy enemy;
+    [SerializeField] int EnemyCount = 3;
+    private void Awake()
+    {
+        for (int i = 0; i < EnemyCount; i++)
+        {
+            int j = Random.Range(0, SpawnPositions.Count);
+            Instantiate(enemy, SpawnPositions[j].position,Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Characters/Player/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    bool DestoryOnTrigger;
+    public int Damage;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy Enemy = collision.GetComponent<Enemy>();
+        if(Enemy == null)
+            Enemy = collision.GetComponentInParent<Enemy>();
+        if (Enemy != null)
+        {
+            HealthComponent healthComponent = Enemy.GetComponent<HealthComponent>();
+            if (healthComponent != null)
+            { 
+                healthComponent.TakeDamage(Damage);
+                if (DestoryOnTrigger)
+                    Destroy(gameObject);
+            }
+        }
+    }
+}
