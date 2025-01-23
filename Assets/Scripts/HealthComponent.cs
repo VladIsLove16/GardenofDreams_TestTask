f8a102bde8ac362406f69f271b0896a3fc3d8c3d
Vladislove <97838524+VladIsLove16@users.noreply.github.com>
2025-01-23 16:18:10 +0300

    health

---
+++ Assets/Scripts/HealthComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    public float health = 100f;
+    public float maxHealth = 100f;
+    public event Action<float, float> HealthChanged;
+    public event Action Died;
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health < 0) health = 0;
+        HealthChanged?.Invoke(health, maxHealth);
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Персонаж умер!");
+        Died?.Invoke();
+    }
+}
---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+[RequireComponent(typeof(HealthComponent))]
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    bool AlwaysVisible;
+    protected VisualElement root;
+    protected ProgressBar healthProgress;
+    VisualElement healthProgressBar;
+    protected HealthComponent healthComponent;
+    #if UNITY_EDITOR
+    [SerializeField]
+    float width;
+    [SerializeField]
+    float offset;
+    [SerializeField]
+    float upper;
+    #endif
+    protected virtual void Awake()
+    {
+        var uiDocument = GetComponent<UIDocument>();
+        root = uiDocument.rootVisualElement;
+
+        healthProgress = root.Q<ProgressBar>("healthProgress");
+        healthProgressBar = healthProgress.Q<VisualElement>("unity-progress-bar");
+
+        healthComponent = GetComponent<HealthComponent>();
+        healthComponent.HealthChanged += UpdateHealthBarProgress;
+
+        UpdatehealthBarWidth();
+        UpdateHealthBarProgress(healthComponent.health, healthComponent.maxHealth);
+        UpdateHealthBarVisibility(healthComponent.health, healthComponent.maxHealth);
+    }
+
+    protected void Update()
+    {
+        UpdateHealthBarPosition();
+        UpdatehealthBarWidth();
+
+    }
+    public void Show()
+    {
+        root.style.visibility = Visibility.Visible;
+    }
+    public void Hide()
+    {
+        root.style.visibility = Visibility.Hidden;
+    }
+    protected void UpdateHealthBarProgress(float health, float maxHealth)
+    {
+        float healthRatio = health / maxHealth;
+        healthProgress.value = healthRatio * 100;
+        UpdateHealthBarVisibility( health,  maxHealth);
+
+    }
+
+    protected virtual void UpdatehealthBarWidth()
+    {
+        healthProgressBar.style.width = width;
+        healthProgressBar.style.maxWidth = width;
+    }
+
+    protected virtual void UpdateHealthBarVisibility(float health, float maxHealth)
+    {
+        if (AlwaysVisible)
+        {
+            Show();
+            return;
+        }
+        if (health == maxHealth)
+        {
+            Hide();
+        }
+        else
+        {
+            Show();
+        }
+    }
+
+    protected virtual void UpdateHealthBarPosition()
+    {
+        Vector3 worldPos = transform.position + Vector3.up * upper;  // Смещаем полоску здоровья выше врага
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+
+        root.style.left = screenPos.x - offset;
+        root.style.top = Screen.height - screenPos.y;  // Инвертируем Y
+    }
+}
